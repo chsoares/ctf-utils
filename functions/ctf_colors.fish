c92818b01c08c32565c0a85237503cb3c87c68ee
chsoares <cordial.road4051@fastmail.com>
2025-07-16 14:51:58 -0300

    updated colors

--- functions/ctf_colors.fish
+++ functions/ctf_colors.fish
@@ -1,3 +1,5 @@
+# special symbols
+
 function ctf_header
     echo (set_color yellow --bold)"  "$argv(set_color normal)
 end
@@ -22,3 +24,31 @@ end
 function ctf_title
     echo (set_color magenta --bold)"  "$argv(set_color normal)
 end
+
+
+## no special symbols
+
+# function ctf_header
+#     echo (set_color yellow --bold)"[+] "$argv(set_color normal)
+# end
+# function ctf_info
+#     echo (set_color cyan)"[*] "$argv(set_color normal)
+# end
+# function ctf_cmd
+#     echo (set_color blue)"[>] "$argv(set_color normal)
+# end
+# function ctf_error
+#     echo (set_color red --bold)"[!] "$argv(set_color normal)
+# end
+# function ctf_warn
+#     echo (set_color magenta --bold)"[*] "$argv(set_color normal)
+# end
+# function ctf_success
+#     echo (set_color magenta --bold)"[✓] "$argv(set_color normal)
+# end
+# function ctf_question
+#     echo (set_color magenta)"[?] "$argv(set_color normal)
+# end
+# function ctf_title
+#     echo (set_color magenta --bold)"[~] "$argv(set_color normal)
+# end
